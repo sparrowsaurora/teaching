b57d820f289961301c14824cb25b1ef3d36be901
Sparrows <35826584+sparrowsaurora@users.noreply.github.com>
2026-03-03 01:34:48 +0800

    wrote chapter 1 of beginner python

---
+++ python-for-beginners/chapter-1/3.basic-strings.py
@@ -0,0 +1,9 @@
+
+# strings are lists of individual characters.
+# In python there is only 1 string type - the 'str' type
+
+"this is a string, its stored nowhere, but it's a string"
+
+# a string in python can be in:
+'single quotes'
+"or double quotes"
---
+++ python-for-beginners/chapter-1/4.variables.py
@@ -0,0 +1,27 @@
+
+# variables in python are just containers
+# that hold ANY value you give it
+
+string_var = "this is a variable holding this string"
+
+# and this is a variable holding a number
+number_var = 5
+
+
+# in python we name variables
+# using snake_case, which means lower case words delimited with underscores
+# you cant start variable with a symbol or a number*
+# *With the except of an underscore, which means the value is meant to be left unused.
+
+this_is_valid = 0
+_so_is_this = 0
+# 2_this_is_not = 0
+
+
+# we also have things called constants.
+# you might be familiar with constants in other languages
+# there is no such thing as a real constant in python
+# however you can imply a value is READ-ONLY to you editor and other devs
+# by naming the variable in all caps
+
+LIKE_THIS = "some string"
