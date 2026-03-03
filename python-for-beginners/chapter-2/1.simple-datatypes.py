88ed1718cf96a8e99194332cdb4b5512bc93db6e
Sparrows <35826584+sparrowsaurora@users.noreply.github.com>
2026-03-03 23:45:40 +0800

    working on beginner python, chapeter 2

---
+++ python-for-beginners/chapter-2/1.simple-datatypes.py
@@ -0,0 +1,18 @@
+# python has 4 simple data types
+
+# integers - whole numbers, like 5
+number = 5
+# floats - decimal numbers, like 2.3
+decimal = 2.3
+# strings - like "this" (things in quotes)
+string = "this"
+# booleans - true/false statements
+# the first letter in True or False in python is always a capital
+boolean = True
+
+# each one of these can be assigned to a variable, simply by stating variable_name = x
+
+# there are other ways to assign number using Hexadecimal and binary, but we wont cover that beyond this example
+# the following are also forms of integer
+bin_var = 0x10100
+hex_var = 0xc0ffee
---
+++ python-for-beginners/chapter-2/3.user-input.py
@@ -0,0 +1,23 @@
+# by now you can store a string in a variable
+# this is similar
+# however instead of placeing the word dierctly.
+# we are going to use a function much like out print statement
+
+user_input = input()
+
+# thats it.
+
+# this will ask for a string from the user
+# and then we assign it to a variable
+
+# we can then print
+
+print("user entered: " + user_input)
+
+
+# we can also ask the user a question within the input statement, so it isnt a blank question
+# simply buy putting text instide the input() function
+_user_input = input("please answer again: ")
+
+
+# try running it in your own.
